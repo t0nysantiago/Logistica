0210d94b797c45040e1db84f2b8505adf192437d
Tony <Tony@Desktop-de-Tony>
2021-07-06 19:25:51 -0300

    fazVenda na classe vendedor

--- src/backend/Vendedor.java
+++ src/backend/Vendedor.java
@@ -1,6 +1,8 @@
 package backend;
 
 import java.io.Serializable;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -8,9 +10,43 @@ import java.io.Serializable;
  */
 public class Vendedor extends Funcionario implements Autenticavel, Serializable{
     
-    public Vendedor(int senha, String Nome, String CPF, int Idade) {
+    public Vendedor(int senha, int codigoIdent, double valorVendas, String Nome, String CPF, int Idade) {
         super(Nome, CPF, Idade);
         this.senha = senha;
+        this.codigoIdent = codigoIdent;
+        this.valorVendas = valorVendas;
+    }
+
+    public double getValorVendas() {
+        return valorVendas;
+    }
+
+    public void setValorVendas(double valorVendas) {
+        this.valorVendas = valorVendas;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public int getCodigoIdent() {
+        return codigoIdent;
+    }
+
+    public void setCodigoIdent(int codigoIdent) {
+        this.codigoIdent = codigoIdent;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
     }
     
     @Override
@@ -19,6 +55,10 @@ public class Vendedor extends Funcionario implements Autenticavel, Serializable{
         }
     
     private int senha;
+    
+    private int codigoIdent;
+    
+    private double valorVendas;
 
     // outros atributos e métodos
 
@@ -32,4 +72,47 @@ public class Vendedor extends Funcionario implements Autenticavel, Serializable{
 
         return true;
     }
+
+
+    public void fazVenda(){
+        
+        BancoDeDados banco = new BancoDeDados();
+        Scanner entrada = new Scanner(System.in);
+        
+        int codVendDigitado;
+        int codProdDigitado;
+        int quantProdDigitado;
+        int auxQuant = 0;
+        
+        codVendDigitado = entrada.nextInt();
+        codProdDigitado = entrada.nextInt();
+        
+        for(int i = 0; i < banco.vendedores.size(); i++){
+            if(banco.vendedores.get(i).getCodigoIdent() == codVendDigitado){
+                
+            for(int j = 0; j < banco.produtos.size(); j++){    
+                if(banco.produtos.get(j).getPreco() == codProdDigitado){
+                    
+                    quantProdDigitado = entrada.nextInt();
+                    
+                    auxQuant = banco.produtos.get(j).getQuantEstoque();
+                    
+                    if(quantProdDigitado > auxQuant){
+                        JOptionPane.showMessageDialog(null, "Produto nao tem estoque suficiente! \n");
+                    }else{
+                        banco.produtos.get(j).setQuantEstoque(auxQuant - quantProdDigitado);
+                        banco.vendedores.get(i).setValorVendas(quantProdDigitado * banco.produtos.get(j).getPreco());
+                    }
+                    
+                    
+                }else{
+                    JOptionPane.showMessageDialog(null, "Produto nao encontrado! \n");
+                }
+                
+            }}else{
+                JOptionPane.showMessageDialog(null, "Vendedor nao encontrado! \n");
+            }
+            }
+        
+}
 }
